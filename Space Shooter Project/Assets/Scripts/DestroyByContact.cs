d68973a97c0a6c4455bcfa504099f3aed014a73f
Zeldronic <60047948+Zeldronic@users.noreply.github.com>
2020-04-19 18:36:48 -0400

    Final Project

---
+++ Space Shooter Project/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+    private PlayerController playerController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script.");
+        }
+
+        GameObject playerControllerObject = GameObject.FindWithTag("Player");
+        if (playerControllerObject != null)
+        {
+            playerController = playerControllerObject.GetComponent<PlayerController>();
+        }
+        if (playerController == null)
+        {
+            Debug.Log("Cannot find 'PlayerController' script.");
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+        {
+            return;
+        }
+
+        if((other.CompareTag("Enemy") && gameObject.tag == "Pickup")||(other.CompareTag("Pickup") && gameObject.tag=="Enemy"))
+        {
+            return;
+        }
+
+        if(explosion!= null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+        
+
+        if (gameObject.tag !="Pickup" && other.CompareTag("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+
+        if (gameObject.tag == "Pickup")
+        {
+           playerController.PowerUp();
+            Destroy(gameObject);
+            return;
+        }
+
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
